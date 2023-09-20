85ed3179e8aa30a37783e40cbb63eed987ed83ca
Lucas Oliveira <lucasaugustooliveira75@gmail.com>
2023-09-19 21:04:41 -0300

    :feat: criando a tela de apontamento de horas

--- ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/controller/SendTimeController.java
+++ ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/controller/SendTimeController.java
@@ -7,6 +7,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -17,15 +18,16 @@ public class SendTimeController {
     private SendTimeService sendTimeService;
 
     @GetMapping
-    public String formRegister() {
-
+    public String formRegister(Model model) {
+        model.addAttribute("rcs", sendTimeService.getAllResult());
+        model.addAttribute("users", sendTimeService.getAllUsers());
         return "timeApontament/timeApontament";
-
     }
 
     @PostMapping
     @Transactional
-    public String sendTime(@RequestBody @Valid ResgistrationSendTimeRecord data) {
+    public String sendTime(@Valid ResgistrationSendTimeRecord data) {
+        System.out.println(data.idUser().toString() + " , " + data.startDate().toString()+ " , " + data.finishDate().toString());
         sendTimeService.saveTime(data);
         return "timeApontament/timeApontament";
 
