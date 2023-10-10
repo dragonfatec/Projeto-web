4c7b77bca8f9a486be95408379427da8549707f7
Lucas Oliveira <lucasaugustooliveira75@gmail.com>
2023-10-09 22:35:31 -0300

    :feat: Aplicando Spring Security e BCrypt, na tela de Login

--- ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/dto/RegistrationUserRecord.java
+++ ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/dto/RegistrationUserRecord.java
@@ -1,12 +1,19 @@
 package com.RP.ControleDeJornada.domain.dto;
 
 import com.RP.ControleDeJornada.domain.entitys.user.JobRole;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
+import java.security.SecureRandom;
 
 public record RegistrationUserRecord(
         @NotBlank
         String name,
+        @Email
+        String email,
+        @NotBlank
+        String password,
         @NotNull
         JobRole jobrole,
         @NotBlank
--- ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/repository/UserRepository.java
+++ ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.RP.ControleDeJornada.domain.repository;
 
 import com.RP.ControleDeJornada.domain.entitys.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    UserDetails findByEmail(String email);
 }
