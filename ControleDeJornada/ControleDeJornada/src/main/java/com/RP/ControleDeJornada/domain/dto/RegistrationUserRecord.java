45808871e6e9282da43d4f7f59796cc05cd63618
Lucas Oliveira <lucasaugustooliveira75@gmail.com>
2023-10-20 19:41:35 -0300

    :refactor: Refatorando método de salvar usuario

--- ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/dto/RegistrationUserRecord.java
+++ ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/dto/RegistrationUserRecord.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.security.SecureRandom;
-
 public record RegistrationUserRecord(
         @NotBlank
         String name,
