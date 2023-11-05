1c1e4d4f77c2b93d18e1fb34c7425fb231ef6f6c
Lucas Oliveira <lucasaugustooliveira75@gmail.com>
2023-11-05 12:18:27 -0300

    :feat: Adicionando relacionamento

--- Front/static/js/timeApontament/getRCByClient.js
+++ Front/static/js/timeApontament/getRCByClient.js
@@ -1,13 +1,29 @@
 const urlRC = "http://localhost:8080/sendtime/getRCByClients";
 
-export default async function getRCByClient(eventRc, cnpj){
+export default async function getRCByClient(eventRc, cnpj, selectId){
     eventRc.preventDefault();
+
+    const select = document.getElementById(selectId);
 
     const response = await fetch(urlRC,{
         method: "POST",
         headers: {
+            "Content-type": "application/json",
             "Authorization": `Bearer ${localStorage.getItem("token")}`
         },
         body: cnpj
     })
+
+    if(response.ok){
+        const data = await response.json();
+        console.log(data)
+        data.map((resp) => {
+            const option = document.createElement("option");
+            option.innerText = resp.rc;
+            option.value = resp.codeRc;
+
+            select.appendChild(option);
+        })
+    }
+    
 }
