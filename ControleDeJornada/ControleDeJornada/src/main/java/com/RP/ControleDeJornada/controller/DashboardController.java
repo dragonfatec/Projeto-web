ea0beb3fec103bccad556808349922af32f00f39
pabloo-cunha <pablo.limacunha@outlook.com>
2023-11-08 21:00:44 -0300

    :feat: config dashboard para RC

--- ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/controller/DashboardController.java
+++ ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/controller/DashboardController.java
@@ -1,18 +1,23 @@
 package com.RP.ControleDeJornada.controller;
 
+import com.RP.ControleDeJornada.domain.entitys.resultCenter.ResultCenter;
 import com.RP.ControleDeJornada.domain.entitys.sendTime.SendTime;
 import com.RP.ControleDeJornada.domain.entitys.user.User;
+import com.RP.ControleDeJornada.domain.repository.RcRepository;
 import com.RP.ControleDeJornada.domain.repository.SendTypeRepository;
 import com.RP.ControleDeJornada.domain.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DashboardController {
 
     SendTypeRepository str;
     UserRepository ur;
+
+    RcRepository rcRepository;
 
     public ResponseEntity<List<SendTime>> findHourToDashboard(Integer idUser){
         User userSelection = ur.findByRegistration(idUser);
@@ -21,5 +26,9 @@ public class DashboardController {
         }
         List<SendTime> hourPerUser = str.findSendTimesByUser(userSelection);
         return ResponseEntity.ok().body(hourPerUser);
+    }
+
+    public ResponseEntity<List<SendTime>> rcHourToDashboard(String idResultCenter){
+        List<ResultCenter> peopleOfRC = rcRepository.findAllById(Collections.singleton(idResultCenter));
     }
 }
--- ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/repository/RcRepository.java
+++ ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/repository/RcRepository.java
@@ -1,7 +1,26 @@
 package com.RP.ControleDeJornada.domain.repository;
 
 import com.RP.ControleDeJornada.domain.entitys.resultCenter.ResultCenter;
+import com.RP.ControleDeJornada.domain.entitys.sendTime.SendTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RcRepository extends JpaRepository<ResultCenter, String> {
+
+
+//    @Query("SELECT " +
+//            "sum(s.budget1601) as b1, " +
+//            "sum (s.budget1602) as b2, " +
+//            "sum(s.budget3000) as b3, " +
+//            "sum(s.budget1809) as b4, " +
+//            "sum(s.budget3001) as b5 " +
+//            "FROM " +
+//            "User us " +
+//            "INNER JOIN tb_user_result_centers"
+//            "WHERE " +
+//            "s.user = :idResultCenter"))
+//    public List<SendTime> findTimeByRC(@Param("idResultCenter")ResultCenter idResultCenter){};
 }
