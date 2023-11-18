2d01e68347dd57a0f697960b45c06f9ae7c0019e
Lukas <lkfernando12345@gmail.com>
2023-11-18 16:03:59 -0300

    :feat: Creating the Export to export the excel

--- ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/entitys/excel/SendTimeExport.java
+++ ControleDeJornada/ControleDeJornada/src/main/java/com/RP/ControleDeJornada/domain/entitys/excel/SendTimeExport.java
@@ -6,37 +6,37 @@ import lombok.Data;
 @Data
 public class SendTimeExport {
 
-    private String startDate;
-    private String finishDate;
-    private String typeSend;
-    private String team;
-    private String nome;
-    private String email;
-    private String client;
-    private double budget1601;
-    private double budget1602;
-    private double budget1809;
-    private double budget3000;
-    private double budget3001;
-    private String justification;
+    private String data_inicial;
+    private String data_final;
+    private String tipo_de_hora;
+    private String centro_de_resultado;
+    private String nome_usuario;
+    private String email_usuario;
+    private String cliente;
+    private double verba_1601;
+    private double verba_1602;
+    private double verba_1809;
+    private double verba_3000;
+    private double verba_3001;
+    private String justificativa;
     private String status;
-    private String approvedStatus;
+//    private String approvedStatus;
 
     public SendTimeExport(SendTime sendTime){
-        this.startDate = sendTime.getStartDate().toString();
-        this.finishDate = sendTime.getFinishDate().toString();
-        this.typeSend = sendTime.getTypeSend().toString();
-        this.team = sendTime.getTeam().getAcronym().toString();
-        this.nome = sendTime.getUser().getName();
-        this.email = sendTime.getUser().getEmail();
-        this.client = sendTime.getClient().getNameCompany();
-        this.budget1601 = sendTime.getBudget1601();
-        this.budget1602 = sendTime.getBudget1602();
-        this.budget1809 = sendTime.getBudget1809();
-        this.budget3000 = sendTime.getBudget3000();
-        this.budget3001 = sendTime.getBudget3001();
-        this.justification = sendTime.getJustification();
+        this.data_inicial = sendTime.getStartDate().toString();
+        this.data_final = sendTime.getFinishDate().toString();
+        this.tipo_de_hora = sendTime.getTypeSend().toString();
+        this.centro_de_resultado = sendTime.getTeam().getAcronym().toString();
+        this.nome_usuario = sendTime.getUser().getName();
+        this.email_usuario = sendTime.getUser().getEmail();
+        this.cliente = sendTime.getClient().getNameCompany();
+        this.verba_1601 = sendTime.getBudget1601();
+        this.verba_1602 = sendTime.getBudget1602();
+        this.verba_1809 = sendTime.getBudget1809();
+        this.verba_3000 = sendTime.getBudget3000();
+        this.verba_3001 = sendTime.getBudget3001();
+        this.justificativa = sendTime.getJustification();
         this.status = sendTime.getStatus().toString();
-        this.approvedStatus = sendTime.getApprovedStatus().toString();
+//        this.approvedStatus = sendTime.getApprovedStatus().toString();
     }
 }
